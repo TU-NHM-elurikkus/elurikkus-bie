fe80dfbf36935a3e2f634c9a7c39562bda10055c
Alar Kume <alarku07@gmail.com>
2017-11-21 09:41:44 +0200

    fix 404 giving crashing errors

--- grails-app/controllers/au/org/ala/bie/ExternalSiteController.groovy
+++ grails-app/controllers/au/org/ala/bie/ExternalSiteController.groovy
@@ -18,12 +18,17 @@ class ExternalSiteController {
 
     def genbankBase = "http://www.ncbi.nlm.nih.gov"
     def scholarBase = "http://scholar.google.com"
+    def eolBase = "http://eol.org/api"
 
     def eol = {
-        def searchString = params.s
+        def searchString = params.s?:""
         def filterString  = java.net.URLEncoder.encode(params.f?:"", "UTF-8")
         def nameEncoded = java.net.URLEncoder.encode(searchString, "UTF-8")
-        def searchURL = "http://eol.org/api/search/1.0.json?q=${nameEncoded}&page=1&exact=true&filter_by_taxon_concept_id=&filter_by_hierarchy_entry_id=&filter_by_string=${filterString}&cache_ttl="
+        if (!nameEncoded) {
+            render (["Error": "Argument 's' is required"] as JSON)
+        }
+
+        def searchURL = "${eolBase}/search/1.0.json?q=${nameEncoded}&page=1&exact=true&filter_by_taxon_concept_id=&filter_by_hierarchy_entry_id=&filter_by_string=${filterString}&cache_ttl="
         def locale = RequestContextUtils.getLocale(request)
         def js = new JsonSlurper()
         def jsonText = new java.net.URL(searchURL).text
@@ -31,14 +36,18 @@ class ExternalSiteController {
         def json = js.parseText(jsonText)
 
         // get first pageId
-        if(json.results){
+        response.setContentType("application/json")
+        if(json.results) {
             def pageId = json.results[0].id
-            def pageUrl = "http://eol.org/api/pages/1.0/${pageId}.json?images=00&videos=0&sounds=0&maps=0&text=2&iucn=false&subjects=overview&licenses=all&details=true&taxonomy=false&vetted=0&language=${locale}&cache_ttl="
-            def pageText = new java.net.URL(pageUrl).text
-            response.setContentType("application/json")
-            render pageText
+            def pageUrl = "${eolBase}/pages/1.0/${pageId}.json?images=00&videos=0&sounds=0&maps=0&text=2&iucn=false&subjects=overview&licenses=all&details=true&taxonomy=false&vetted=0&language=${locale}&cache_ttl="
+
+            try {
+                def pageText = new java.net.URL(pageUrl).getText("UTF-8")
+                render pageText
+            } catch (FileNotFoundException err) {
+                render (["Error": err.getMessage()] as JSON)
+            }
         } else {
-            response.setContentType("application/json")
             render ([:] as JSON)
         }
     }
