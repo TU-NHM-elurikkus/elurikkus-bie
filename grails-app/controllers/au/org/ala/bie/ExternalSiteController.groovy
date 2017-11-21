63b66840bd906e6fe4a55c813aca3a4438b505e2
Alar Kume <alarku07@gmail.com>
2017-11-21 10:07:49 +0200

    style; also catch 500 error

--- grails-app/controllers/au/org/ala/bie/ExternalSiteController.groovy
+++ grails-app/controllers/au/org/ala/bie/ExternalSiteController.groovy
@@ -21,34 +21,31 @@ class ExternalSiteController {
     def eolBase = "http://eol.org/api"
 
     def eol = {
-        def searchString = params.s?:""
-        def filterString  = java.net.URLEncoder.encode(params.f?:"", "UTF-8")
+        def searchString = params.s ?: ""
+        def filterString  = java.net.URLEncoder.encode(params.f ?: "", "UTF-8")
         def nameEncoded = java.net.URLEncoder.encode(searchString, "UTF-8")
-        if (!nameEncoded) {
-            render (["Error": "Argument 's' is required"] as JSON)
-        }
 
+        def js = new JsonSlurper()
         def searchURL = "${eolBase}/search/1.0.json?q=${nameEncoded}&page=1&exact=true&filter_by_taxon_concept_id=&filter_by_hierarchy_entry_id=&filter_by_string=${filterString}&cache_ttl="
         def locale = RequestContextUtils.getLocale(request)
-        def js = new JsonSlurper()
-        def jsonText = new java.net.URL(searchURL).text
 
-        def json = js.parseText(jsonText)
-
-        // get first pageId
         response.setContentType("application/json")
-        if(json.results) {
-            def pageId = json.results[0].id
-            def pageUrl = "${eolBase}/pages/1.0/${pageId}.json?images=00&videos=0&sounds=0&maps=0&text=2&iucn=false&subjects=overview&licenses=all&details=true&taxonomy=false&vetted=0&language=${locale}&cache_ttl="
+        try {
+            def jsonText = new java.net.URL(searchURL).getText("UTF-8")
+            def json = js.parseText(jsonText)
 
-            try {
+            // get first pageId
+            if(json.results) {
+                def pageId = json.results[0].id
+                def pageUrl = "${eolBase}/pages/1.0/${pageId}.json?images=00&videos=0&sounds=0&maps=0&text=2&iucn=false&subjects=overview&licenses=all&details=true&taxonomy=false&vetted=0&language=${locale}&cache_ttl="
+
                 def pageText = new java.net.URL(pageUrl).getText("UTF-8")
                 render pageText
-            } catch (FileNotFoundException err) {
-                render (["Error": err.getMessage()] as JSON)
+            } else {
+                render ([:] as JSON)
             }
-        } else {
-            render ([:] as JSON)
+        } catch (IOException | FileNotFoundException err) {
+            render (["Error": err.getMessage()] as JSON)
         }
     }
 
